cf08fe4856421bab5c98c03e0972657c1fb168dd
Jehy <fate@jehy.ru>
2014-03-03 15:46:45 +0400

    add adsense to banner redirect

--- docs/index.php
+++ docs/index.php
@@ -72,13 +72,28 @@ if ((isset($from)) && (isset($rand))) //perehod
       }</style>
       <LINK REL="SHORTCUT ICON" HREF="<?=COMMON_URL;?>favicon.ico">
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-      <META http-equiv="refresh" content="2; URL=<?=$row['url'];?>">
+      <META http-equiv="refresh" content="5; URL=<?=$row['url'];?>">
   </head>
   <body>
   <div align="center">
+        
+        <div style="height:30px;"> &nbsp; </div>
       Большое спасибо за использование баннерной системы <a target="_blank" href="<?=COMMON_URL;?>">"Палантир"</a><br>
-      Через две секунды вы будете направлены на сайт "<?=$row['name']?>";<br>
+      Через пять секунд вы будете направлены на сайт "<?=$row['name']?>";<br>
       Если этого не случилось - нажмите <a href="<?=$row['url']?>">на эту ссылку.</a></div>
+        
+        <div style="height:30px;"> &nbsp; </div>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- header ad -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:728px;height:90px"
+     data-ad-client="ca-pub-3934345222756824"
+     data-ad-slot="8654483999"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div style="height:30px;"> &nbsp; </div>
+  
   </body>
   </html><?die();
   }
