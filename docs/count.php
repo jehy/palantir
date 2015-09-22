c8e963d2e2a16583284c070142a1fe0275181406
Jehy <fate@jehy.ru>
2015-09-22 16:24:19 +0300

    many bugfixes and improvements

--- docs/count.php
+++ docs/count.php
@@ -1,16 +1,33 @@
 <?
-error_reporting(E_ALL);
-$id = $_REQUEST['id'];
-$referer = $_REQUEST['referer'];
-$img = $_REQUEST['img'];
-$pic = $_REQUEST['pic'];
+error_reporting(E_ALL & ~E_NOTICE);
+$id = $_GET['id'];
+$referer = $_GET['referer'];
+$img = $_GET['img'];
+$pic = $_GET['pic'];
+$cid = $_GET['cid'];
+$today_hits = $_GET['today_hits'];
+$total_hits = $_GET['total_hits'];
+$today_hosts = $_GET['today_hosts'];
+$total_hosts = $_GET['total_hosts'];
+
 if (!mb_check_encoding($referer, 'UTF-8'))
     $referer = mb_convert_encoding($referer, 'UTF-8');
 if ((!strpos($img, '.')) && (($img != ''))) $img .= '.png';
 if ((!strpos($pic, '.')) && (($pic != ''))) $pic .= '.png';
 $ref = $_SERVER['HTTP_REFERER'];
 $agent = $_SERVER['HTTP_USER_AGENT'];
-$ip_w = sprintf("%u", ip2long($_SERVER['REMOTE_ADDR']));
+$ip_w = ip2long($_SERVER['REMOTE_ADDR']);
+
+function cnt_show_error($error)
+{
+    $im = imageCreate(88, 31);
+    $textcol = imagecolorallocate($im, 0, 0, 0);
+    $back = imagecolorallocate($im, 111, 127, 143);
+    imagefill($im, 0, 0, $back);
+    imagestring($im, 5, 0, 00, $error, $textcol);
+    imagepng($im);
+    die();
+}
 
 $errors = '';
 
@@ -26,7 +43,6 @@ $stmt->bind_param('si', $ip_w, $id);
 $stmt->execute();
 $result = $stmt->get_result();
 
-$set = 'update sources set ';
 if ($cid != '') {
     if (!$today_hits && !$total_hits && !$today_hosts && !$total_hosts) {
         $c = $cid . ' - static ';
@@ -43,36 +59,29 @@ if ($cid != '') {
     $c = $img . $pic . $cid;
 }
 
-$uphits = 'today_hits=today_hits+1,total_hits=total_hits+1,cid=?';
+$sql = 'update sources set today_hits=today_hits+1,total_hits=total_hits+1,cid=?';
 $uphosts = 'today_hosts=today_hosts+1, total_hosts=total_hosts+1';
 if (!$result->num_rows) {
     $time = date("H");
-    $sql = 'INSERT INTO addition VALUES (NULL,?,?,?,?,?,?)';
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('ssssii', $ip_w, $ref, $referer, $agent, $id, $time);
+    $sql2 = 'INSERT INTO addition VALUES (NULL,?,?,?,?,?,?)';
+    $stmt = $mysqli->prepare($sql2);
+    $stmt->bind_param('isssii', $ip_w, $ref, $referer, $agent, $id, $time);
     $stmt->execute();
-    $sql = $set . $uphits . ', ' . $uphosts;
-} else $sql = $set . $uphits;
+    $sql .= ', ' . $uphosts;
+}
 $sql = $sql . ' where id=? limit 1';
 $stmt = $mysqli->prepare($sql);
 $stmt->bind_param('si', $cid, $id);
 $stmt->execute();
 
 if ($mysqli->affected_rows != 1)
-    $errors = $errors . "1";
-if ($errors) {
-    $im = imageCreate(88, 31);
-    $textcol = imagecolorallocate($im, 0, 0, 0);
-    $back = imagecolorallocate($im, 111, 127, 143);
-    imagefill($im, 0, 0, $back);
-    imagestring($im, 5, 0, 00, '  errors' . $errors, $textcol);
-    imagepng($im);
-} else {
+    cnt_show_error('site not found');
+else {
     $mirror = get_mirror();
     ##################   OLD, SAVED FOR COMPATIBILITY
-    if (@$_GET['pic']) {
-        Header('location: ' . $mirror['url'] . 'counters/standard/' . @$pic);
-    } elseif (@$_GET['img']) {
+    if ($pic) {
+        Header('location: ' . $mirror['url'] . 'counters/standard/' . $pic);
+    } elseif ($img) {
         $sql = 'select today_hosts,today_hits,total_hosts,total_hits from sources where id=? limit 1';
         $stmt = $mysqli->prepare($sql);
         $stmt->bind_param('i', $id);
@@ -82,9 +91,9 @@ if ($errors) {
             $row = $result->fetch_array(MYSQLI_ASSOC);
         Header('location: ' . $mirror['url'] . 'counters/count.php?cid=' . $img . '&today_hosts=' . $row['today_hosts'] . '&total_hosts=' . $row['total_hosts']);
     } ####################   NEW
-    elseif (@$cid) {
+    elseif ($cid) {
         if (!$today_hits && !$total_hits && !$today_hosts && !$total_hosts) {
-            Header('location: ' . $mirror['url'] . 'counters/standard/' . @$cid);
+            Header('location: ' . $mirror['url'] . 'counters/standard/' . $cid);
         } else {
             $sql = 'select today_hosts,today_hits,total_hosts,total_hits from sources where id=? limit 1';
             $stmt = $mysqli->prepare($sql);
@@ -95,9 +104,9 @@ if ($errors) {
                 $row = $result->fetch_array(MYSQLI_ASSOC);
             if ($today_hosts & $today_hits & $total_hosts) {
                 Header('location: ' . $mirror['url'] . 'counters/count.php?cid=' . $cid . '&today_hits=' . $row['today_hits'] . '&today_hosts=' . $row['today_hosts'] . '&total_hosts=' . $row['total_hosts']);
-            } elseif ($today_hosts & $total_hosts)
+            } else#if ($today_hosts & $total_hosts)
                 Header('location: ' . $mirror['url'] . 'counters/count.php?cid=' . $cid . '&today_hosts=' . $row['today_hosts'] . '&total_hosts=' . $row['total_hosts']);
         }
     }
 }
-?>
+?>lol hi
